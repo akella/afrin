e04d0d5c8d1bec4360dc45fc3a51eab9019ce0d4
akella <akella.a@gmail.com>
2013-05-17 13:06:19 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
 
 
-var i;
-for (i in document.images) {
-    if (document.images[i].src) {
-        var imgSrc = document.images[i].src;
-        if (imgSrc.substr(imgSrc.length-4) === '.png' || imgSrc.substr(imgSrc.length-4) === '.PNG') {
-            document.images[i].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enabled='true',sizingMethod='crop',src='" + imgSrc + "')";
-        }
-    }
-}
+
 
 	$('.cycle').cycle({ 
 		fx:     'fade', 
-		delay:  -5000 
+		delay:  -5000,
+		cleartypeNoBg: true
 	});
 
 	// location dropdown list after yandex-map
